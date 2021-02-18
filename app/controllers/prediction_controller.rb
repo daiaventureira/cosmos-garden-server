1c29da3a7e2d4a2118f8bf28d609a4493c71d0b8
daiaventureira <daiane.souzas@icloud.com>
2021-02-17 23:41:13 -0500

    authorize cors controll

--- app/controllers/prediction_controller.rb
+++ app/controllers/prediction_controller.rb
@@ -3,7 +3,15 @@ class PredictionController < ApplicationController
         Prediction.all
     end
 
+    def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+
     def show
+        cors_set_access_control_headers
         data = Prediction.select(:message, :sign).where(date: Date.parse(params[:date]))
         render :json => data
     end
