77e2f80acf1a57174c81533ca20c3ff78b58d749
daiaventureira <daiane.souzas@icloud.com>
2021-02-17 23:04:37 -0500

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+    get "/", to: "prediction#index"
     get "/prediction/:date", to: "prediction#show", param: :date
     # match '/predictions/:date' => 'prediction#get_prediction', :via => :get
 
