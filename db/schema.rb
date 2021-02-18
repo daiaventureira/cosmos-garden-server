6d2362f5edbb7cc0fe6f1d2d50f80be2583512bc
daiaventureira <daiane.souzas@icloud.com>
2021-02-17 22:43:03 -0500

    migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_17_205517) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "predictions", force: :cascade do |t|
     t.text "message"
     t.integer "sign"
